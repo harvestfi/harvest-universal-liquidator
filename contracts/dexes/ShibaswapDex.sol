a9d5398bade5c82d7c90c6aee0bb8e7bc18d6057
CryptJS13 <jasper@slimmens.nl>
2022-07-06 21:17:55 +0700

    Add all existing dexes, add deployments file, add licenses

---
+++ contracts/dexes/ShibaswapDex.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "./UniBasedDex.sol";
+
+contract ShibaswapDex is
+  UniBasedDex(0x03f7724180AA6b939894B5Ca4314783B0b36b329, 0x115934131916C8b277DD010Ee02de363c09d037c)
+{}
---
+++ contracts/dexes/SushiswapDex.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "./UniBasedDex.sol";
+
+contract SushiswapDex is
+  UniBasedDex(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F, 0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac)
+{}
